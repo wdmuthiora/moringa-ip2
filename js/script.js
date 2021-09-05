c26fd30eddf41964ed79e3f1524f0e6ba5d36f6a
wdmuthiora <wdmuthiora@gmail.com>
2021-09-05 19:36:10 +0300

    Added Js variables, and arrays to be used.

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let century = document.getElementById('year').value;
+console.log(century)
+
+// variables
+// let year = document.getElementById('year.slice(2,4)').value;
+// let month = document.getElementById('month').value;
+// let dayOfMonth = document.getElementById('dayOfMonth').value;
+
+// arrays
+daysOfTheWeek = [Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday];
+maleNames = [Kwasi, Kwodwo, Kwabena, Kwaku, Yaw, Kofi, Kwame];
+femaleNames = [Akosua, Adwoa, Abenaa, Akua, Yaa, Afua, Ama]
