3a45a048b5aedae437952cdd2435c586c161e0d0
wdmuthiora <wdmuthiora@gmail.com>
2021-09-08 13:18:40 +0300

    Added working JS, but testing appers to returs falsy results.

--- js/script.js
+++ js/script.js
@@ -1,35 +1,31 @@
 function akanName() {
-  // // variables to be used
+  // variables to be used
 
   var dayOfMonth = document.getElementById('dayOfMonth').value;
-  alert(dayOfMonth);
-
   var month = document.getElementById('month').value;
-  alert(month);
-
   var yearOfBirth = document.getElementById('year').value;
-  alert(yearOfBirth);
-
-  var female = document.getElementById('gender').value;
-  alert(female);
-
-  var male = document.getElementById('gender').value;
-  alert(male);
-
+  var gender = document.getElementById('gender').value;
   var century = yearOfBirth.slice(0, 2);
-  alert(century);
-
   var year = yearOfBirth.slice(2, 4);
-  alert(year);
 
-  // // arrays to be used
+
+  // arrays to be used
   daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
   maleNames = ['Kwasi', 'Kwodwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
 
   femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
 
+  //formula to determine day of the week
+  var day = Math.ceil((((century / 4) - 2 * century - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + dayOfMonth) % 7)
 
-  // (((century / 4) - 2 * century - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + day) mod 7
-
+  alert(day)
+  var dayBorn = daysOfTheWeek[day - 1];
+  var femaleAkanName = femaleNames[day - 1];
+  var maleAkanName = maleNames[day - 1];
+  if (gender === "Male") {
+    alert("Your name is " + maleAkanName + " and you were born on " + dayBorn)
+  } else {
+    alert("Your name is " + femaleAkanName + " and you were born on " + dayBorn)
+  }
 }
